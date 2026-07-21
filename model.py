ab96ccf34e55b88cbc582f788699fb2d514b2b63
jeanjosephbezef <jjiguetj@icloud.com>
2026-07-22 01:07:39 +0200

    Delete model.py

--- model.py
+++
@@ -1,8 +0,0 @@
-from database import db
-
-
-class Produit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.String(100), nullable=False)
-    prix = db.Column(db.Float, nullable=False)
-    stock = db.Column(db.Integer, default=0)
