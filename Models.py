f41ad766a3b6efa9a0eee7ee05aea005ef90ebec
jeanjosephbezef <jjiguetj@icloud.com>
2026-07-22 01:03:44 +0200

    Create Produit model for product management

---
+++ Models.py
@@ -0,0 +1,8 @@
+from database import db
+
+
+class Produit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100), nullable=False)
+    prix = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, default=0)
